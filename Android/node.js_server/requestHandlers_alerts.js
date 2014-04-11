784d95173d22815c12050a08a7c0db3b9fa03e23
Andy Aspell-Clark <andyaspellclark@gmail.com>
2014-04-11 12:11:19 +0100

    removed node.js test server code

--- Android/node.js_server/requestHandlers_alerts.js
+++
@@ -1,9 +0,0 @@
-var exec = require("child_process").exec;
-var querystring = require("querystring");
-var fs = require("fs");
-var formidable = require('formidable');
-var sys = require('sys');
-
-//var database = require("./database");
-
-
