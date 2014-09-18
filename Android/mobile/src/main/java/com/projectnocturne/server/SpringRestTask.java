15ec09871e885686db95645419743611b492b6ba
Andy Aspell-Clark <andyaspellclark@gmail.com>
2014-09-18 13:13:50 +0100

    removed tv and wear modules

--- Android/mobile/src/main/java/com/projectnocturne/server/SpringRestTask.java
+++ Android/mobile/src/main/java/com/projectnocturne/server/SpringRestTask.java
@@ -44,166 +44,158 @@ import org.springframework.web.client.RestTemplate;
 
 /**
  * @author andy
- *
  */
 public final class SpringRestTask extends AsyncTask<Object, String, RESTResponseMsg> {
 
-	public enum RequestMethod {
-		GET, POST
-	}
+    public static final String URI_SEND_SENSOR_READING = "/sensors/reading";
+    public static final Object URI_USER_CHECK_STATUS = "/users/user_check_status";
+    private static final String LOG_TAG = SpringRestTask.class.getSimpleName() + "::";
+    public static String URI_ALERTS_RESPOND = "/alerts/respond";
+    public static String URI_ALERTS_SEND = "/alerts/send";
+    public static String URI_USER_REGISTER = "/users/register";
+    public static String URI_USERS_GET = "/users";
+    private final Context ctx;
+    private final Handler handler;
+    private String uri;
+    private String url;
 
-	private static final String LOG_TAG = SpringRestTask.class.getSimpleName() + "::";
-	public static String URI_ALERTS_RESPOND = "/alerts/respond";
-	public static String URI_ALERTS_SEND = "/alerts/send";
-	public static final String URI_SEND_SENSOR_READING = "/sensors/reading";
-	public static final Object URI_USER_CHECK_STATUS = "/users/user_check_status";
-	public static String URI_USER_REGISTER = "/users/register";
-	public static String URI_USERS_GET = "/users";
+    public SpringRestTask(final Context contxt, final Handler hndlr) {
+        ctx = contxt;
+        handler = hndlr;
+    }
 
-	private final Context ctx;
+    /*
+     * (non-Javadoc)
+     *
+     * @see android.os.AsyncTask#doInBackground(java.lang.Object[])
+     */
+    @Override
+    protected RESTResponseMsg doInBackground(final Object... params) {
 
-	private final Handler handler;
-	private String uri;
+        final RequestMethod reqMthd = RequestMethod.valueOf(params[0].toString());
+        uri = params[1].toString();
+        url = getServerAddress(ctx) + uri;
 
-	private String url;
+        RESTResponseMsg retStr = null;
+        if (uri.equals(URI_USER_REGISTER)) {
+            retStr = doUserRegister(reqMthd, url, (UserDb) params[2]);
+        } else if (uri.equals(URI_USERS_GET)) {
+            // retStr = doUserRegister(reqMthd, url, (User) params[2]);
+        } else if (uri.equals(URI_SEND_SENSOR_READING)) {
+            retStr = doSendSensorReading(reqMthd, url, (SensorReading) params[2]);
+        }
+        return retStr;
+    }
 
-	public SpringRestTask(final Context contxt, final Handler hndlr) {
-		ctx = contxt;
-		handler = hndlr;
-	}
+    /**
+     * @param reqMthd
+     * @param uri
+     * @param sensorReading
+     * @return
+     */
+    private RESTResponseMsg doSendSensorReading(final RequestMethod reqMthd, final String url, final SensorReading sensorReading) {
+        NocturneApplication.d(LOG_TAG + "doSendSensorReading()");
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see android.os.AsyncTask#doInBackground(java.lang.Object[])
-	 */
-	@Override
-	protected RESTResponseMsg doInBackground(final Object... params) {
+        // Create a new RestTemplate instance
+        final RestTemplate restTemplate = new RestTemplate();
 
-		final RequestMethod reqMthd = RequestMethod.valueOf(params[0].toString());
-		uri = params[1].toString();
-		url = getServerAddress(ctx) + uri;
+        // Add the Jackson and String message converters
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
 
-		RESTResponseMsg retStr = null;
-		if (uri.equals(URI_USER_REGISTER)) {
-			retStr = doUserRegister(reqMthd, url, (UserDb) params[2]);
-		} else if (uri.equals(URI_USERS_GET)) {
-			// retStr = doUserRegister(reqMthd, url, (User) params[2]);
-		} else if (uri.equals(URI_SEND_SENSOR_READING)) {
-			retStr = doSendSensorReading(reqMthd, url, (SensorReading) params[2]);
-		}
-		return retStr;
-	}
+        // Make the HTTP POST request, marshaling the request to JSON, and the
+        // response to a String
+        final RESTResponseMsg response = restTemplate.postForObject(url, sensorReading, RESTResponseMsg.class);
 
-	/**
-	 * @param reqMthd
-	 * @param uri
-	 * @param sensorReading
-	 * @return
-	 */
-	private RESTResponseMsg doSendSensorReading(final RequestMethod reqMthd, final String url,
-			final SensorReading sensorReading) {
-		NocturneApplication.d(LOG_TAG + "doSendSensorReading()");
+        return response;
+    }
 
-		// Create a new RestTemplate instance
-		final RestTemplate restTemplate = new RestTemplate();
+    /**
+     * @param reqMthd
+     * @param uri
+     * @param object
+     */
+    private RESTResponseMsg doUserRegister(final RequestMethod reqMthd, final String url, final UserDb user) {
+        NocturneApplication.d(LOG_TAG + "doUserRegister()");
 
-		// Add the Jackson and String message converters
-		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-		restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+        String jsonStr = "";
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+            jsonStr = mapper.writeValueAsString(user.getUserObj());
+        } catch (final JsonProcessingException e1) {
+            NocturneApplication.e(LOG_TAG + "doUserRegister() exception converting user to json", e1);
+        }
 
-		// Make the HTTP POST request, marshaling the request to JSON, and the
-		// response to a String
-		final RESTResponseMsg response = restTemplate.postForObject(url, sensorReading, RESTResponseMsg.class);
+        // Create a new RestTemplate instance
+        final RestTemplate restTemplate = new RestTemplate();
 
-		return response;
-	}
+        // Add the JSON and String message converters
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
 
-	/**
-	 * @param reqMthd
-	 * @param uri
-	 * @param object
-	 */
-	private RESTResponseMsg doUserRegister(final RequestMethod reqMthd, final String url, final UserDb user) {
-		NocturneApplication.d(LOG_TAG + "doUserRegister()");
+        // Make the HTTP POST request, marshaling the request to JSON, and the
+        // response to a String
+        RESTResponseMsg response = null;
+        Message msg;
+        try {
+            response = restTemplate.postForObject(url, user.getUserObj(), RESTResponseMsg.class);
+            msg = handler.obtainMessage(DbMetadata.RegistrationStatus_ACCEPTED);
+            final Bundle b = new Bundle();
+            b.putParcelable("RESTResponseMsg", response);
+            msg.setData(b);
+            NocturneApplication.getInstance().getDataModel().setRegistrationStatus(RegistrationStatus.REQUEST_ACCEPTED);
+        } catch (final Exception e) {
+            NocturneApplication.e(LOG_TAG + "doUserRegister() exception posting request", e);
+            NocturneApplication.getInstance().getDataModel().setRegistrationStatus(RegistrationStatus.REQUEST_DENIED);
+            msg = handler.obtainMessage(DbMetadata.RegistrationStatus_DENIED);
+        }
+        handler.sendMessage(msg);
 
-		String jsonStr = "";
-		try {
-			final ObjectMapper mapper = new ObjectMapper();
-			mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-			jsonStr = mapper.writeValueAsString(user.getUserObj());
-		} catch (final JsonProcessingException e1) {
-			NocturneApplication.e(LOG_TAG + "doUserRegister() exception converting user to json", e1);
-		}
+        return response;
+    }
 
-		// Create a new RestTemplate instance
-		final RestTemplate restTemplate = new RestTemplate();
+    private String getServerAddress(final Context ctx) {
+        final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(ctx);
+        String serverIpAddr = SettingsActivity.PREF_SERVER_ADDRESS_DEFAULT;
+        serverIpAddr = "10.0.4.227";
+        final String serverAddr = "http://" + settings.getString(SettingsActivity.PREF_SERVER_ADDRESS, serverIpAddr) + ":" + settings.getString(SettingsActivity.PREF_SERVER_PORT, SettingsActivity.PREF_SERVER_PORT_DEFAULT) + "/";
+        return serverAddr;
+    }
 
-		// Add the JSON and String message converters
-		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-		restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+    /*
+     * (non-Javadoc)
+     *
+     * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+     */
+    @Override
+    protected void onPostExecute(final RESTResponseMsg result) {
+        NocturneApplication.d(LOG_TAG + "onPostExecute(" + (result == null ? "" : result.getStatus()) + ")");
+        super.onPostExecute(result);
+    }
 
-		// Make the HTTP POST request, marshaling the request to JSON, and the
-		// response to a String
-		RESTResponseMsg response = null;
-		Message msg;
-		try {
-			response = restTemplate.postForObject(url, user.getUserObj(), RESTResponseMsg.class);
-			msg = handler.obtainMessage(DbMetadata.RegistrationStatus_ACCEPTED);
-			final Bundle b = new Bundle();
-			b.putParcelable("RESTResponseMsg", response);
-			msg.setData(b);
-			NocturneApplication.getInstance().getDataModel().setRegistrationStatus(RegistrationStatus.REQUEST_ACCEPTED);
-		} catch (final Exception e) {
-			NocturneApplication.e(LOG_TAG + "doUserRegister() exception posting request", e);
-			NocturneApplication.getInstance().getDataModel().setRegistrationStatus(RegistrationStatus.REQUEST_DENIED);
-			msg = handler.obtainMessage(DbMetadata.RegistrationStatus_DENIED);
-		}
-		handler.sendMessage(msg);
+    /*
+     * (non-Javadoc)
+     *
+     * @see android.os.AsyncTask#onPreExecute()
+     */
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
 
-		return response;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see android.os.AsyncTask#onProgressUpdate(java.lang.Object[])
+     */
+    @Override
+    protected void onProgressUpdate(final String... values) {
+        super.onProgressUpdate(values);
+    }
 
-	private String getServerAddress(final Context ctx) {
-		final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(ctx);
-		final String serverAddr = "http://"
-				+ settings
-						.getString(SettingsActivity.PREF_SERVER_ADDRESS, SettingsActivity.PREF_SERVER_ADDRESS_DEFAULT)
-				+ ":"
-				+ settings.getString(SettingsActivity.PREF_SERVER_PORT, SettingsActivity.PREF_SERVER_PORT_DEFAULT)
-				+ "/";
-		return serverAddr;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
-	 */
-	@Override
-	protected void onPostExecute(final RESTResponseMsg result) {
-		NocturneApplication.d(LOG_TAG + "onPostExecute(" + (result == null ? "" : result.getStatus()) + ")");
-		super.onPostExecute(result);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see android.os.AsyncTask#onPreExecute()
-	 */
-	@Override
-	protected void onPreExecute() {
-		super.onPreExecute();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see android.os.AsyncTask#onProgressUpdate(java.lang.Object[])
-	 */
-	@Override
-	protected void onProgressUpdate(final String... values) {
-		super.onProgressUpdate(values);
-	}
+    public enum RequestMethod {
+        GET, POST
+    }
 
 }
