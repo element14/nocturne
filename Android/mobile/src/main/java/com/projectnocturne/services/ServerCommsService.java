f4846fa279449e31316a635f8a34f05d95f0a5ef
andy <andyaspellclark@gmail.com>
2014-10-30 05:59:08 +0000

    adding get user list function

--- Android/mobile/src/main/java/com/projectnocturne/services/ServerCommsService.java
+++ Android/mobile/src/main/java/com/projectnocturne/services/ServerCommsService.java
@@ -40,71 +40,86 @@ import org.apache.http.NameValuePair;
 import java.util.List;
 
 public final class ServerCommsService {
-	private static final String LOG_TAG = ServerCommsService.class.getSimpleName() + "::";
+    private static final String LOG_TAG = ServerCommsService.class.getSimpleName() + "::";
 
-	public void checkUserStatus(final Context ctx, final Handler handler, final User obj) {
-		NocturneApplication.logMessage(Log.INFO, LOG_TAG + "checkUserStatus()");
+    public void checkUserStatus(final Context ctx, final Handler handler, final User obj) {
+        NocturneApplication.logMessage(Log.INFO, LOG_TAG + "checkUserStatus()");
 
-		final List<NameValuePair> uriData = RestUriFactory.getUri(RestUriType.CHECK_USER_STATUS, obj);
+        final List<NameValuePair> uriData = RestUriFactory.getUri(RestUriType.CHECK_USER_STATUS, obj);
 
-		if (uriData.size() == 0) {
-			NocturneApplication.logMessage(Log.ERROR, LOG_TAG + "checkUserStatus() for " + obj.getUsername());
-			return;
-		}
+        if (uriData.size() == 0) {
+            NocturneApplication.logMessage(Log.ERROR, LOG_TAG + "checkUserStatus() for " + obj.getUsername());
+            return;
+        }
 
-		final SpringRestTask restReq = new SpringRestTask(ctx, handler);
-		restReq.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, SpringRestTask.RequestMethod.POST.toString(),
-				SpringRestTask.URI_USER_CHECK_STATUS, obj);
-	}
+        final SpringRestTask restReq = new SpringRestTask(ctx, handler);
+        restReq.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, SpringRestTask.RequestMethod.POST.toString(),
+                SpringRestTask.URI_USER_CHECK_STATUS, obj);
+    }
 
-	private String getServerAddress(final Context ctx) {
-		// getting preferences from a specified file
-		final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(ctx);
-		final String serverAddr = "http://"
-				+ settings
-						.getString(SettingsActivity.PREF_SERVER_ADDRESS, SettingsActivity.PREF_SERVER_ADDRESS_DEFAULT)
-				+ ":"
-				+ settings.getString(SettingsActivity.PREF_SERVER_PORT, SettingsActivity.PREF_SERVER_PORT_DEFAULT)
-				+ "/";
-		return serverAddr;
-	}
+    private String getServerAddress(final Context ctx) {
+        // getting preferences from a specified file
+        final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(ctx);
+        final String serverAddr = "http://"
+                + settings
+                .getString(SettingsActivity.PREF_SERVER_ADDRESS, SettingsActivity.PREF_SERVER_ADDRESS_DEFAULT)
+                + ":"
+                + settings.getString(SettingsActivity.PREF_SERVER_PORT, SettingsActivity.PREF_SERVER_PORT_DEFAULT)
+                + "/";
+        return serverAddr;
+    }
 
-	public void sendAlert(final Context ctx, final Handler handler, final Alert obj) {
-		NocturneApplication.logMessage(Log.INFO, LOG_TAG + "sendAlert() " + obj.alert_name);
+    public void getUserList(final Context ctx, final Handler handler) {
+        NocturneApplication.logMessage(Log.INFO, LOG_TAG + "getUserList()");
 
-		final List<NameValuePair> uriData = RestUriFactory.getUri(RestUriType.SEND_ALERT, obj);
+        final List<NameValuePair> uriData = RestUriFactory.getUri(RestUriType.CHECK_USER_STATUS, obj);
 
-		if (uriData.size() == 0) {
-			NocturneApplication.logMessage(Log.ERROR, LOG_TAG + "sendAlert() for " + obj.alert_name);
-			return;
-		}
+        if (uriData.size() == 0) {
+            NocturneApplication.logMessage(Log.ERROR, LOG_TAG + "checkUserStatus() for " + obj.getUsername());
+            return;
+        }
 
-		final HttpRequestTask restReq = new HttpRequestTask();
-		final String serverAddr = getServerAddress(ctx);
-		restReq.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, SpringRestTask.RequestMethod.POST.toString(),
-				serverAddr + "alert_from_patient", uriData);
-	}
+        final SpringRestTask restReq = new SpringRestTask(ctx, handler);
+        restReq.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, SpringRestTask.RequestMethod.POST.toString(),
+                SpringRestTask.URI_USER_CHECK_STATUS, obj);
+    }
 
-	public void sendSensorReading(final Context ctx, final Handler handler, final Sensor obj) {
-		NocturneApplication.logMessage(Log.INFO, LOG_TAG + "sendSensorReading() " + obj.sensor_name);
+    public void sendAlert(final Context ctx, final Handler handler, final Alert obj) {
+        NocturneApplication.logMessage(Log.INFO, LOG_TAG + "sendAlert() " + obj.alert_name);
 
-		final List<NameValuePair> uriData = RestUriFactory.getUri(RestUriType.SEND_SENSOR_READING, obj);
+        final List<NameValuePair> uriData = RestUriFactory.getUri(RestUriType.SEND_ALERT, obj);
 
-		if (uriData.size() == 0) {
-			NocturneApplication.logMessage(Log.ERROR, LOG_TAG + "sendSensorReading() for " + obj.sensor_name);
-			return;
-		}
+        if (uriData.size() == 0) {
+            NocturneApplication.logMessage(Log.ERROR, LOG_TAG + "sendAlert() for " + obj.alert_name);
+            return;
+        }
 
-		final SpringRestTask restReq = new SpringRestTask(ctx, handler);
-		restReq.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, SpringRestTask.RequestMethod.POST.toString(),
-				SpringRestTask.URI_SEND_SENSOR_READING, obj);
-	}
+        final HttpRequestTask restReq = new HttpRequestTask();
+        final String serverAddr = getServerAddress(ctx);
+        restReq.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, SpringRestTask.RequestMethod.POST.toString(),
+                serverAddr + "alert_from_patient", uriData);
+    }
 
-	public void sendSubscriptionMessage(final Context ctx, final Handler handler, final UserDb user) {
-		NocturneApplication.logMessage(Log.INFO, LOG_TAG + "sendSubscriptionMessage() for " + user.getName_first());
+    public void sendSensorReading(final Context ctx, final Handler handler, final Sensor obj) {
+        NocturneApplication.logMessage(Log.INFO, LOG_TAG + "sendSensorReading() " + obj.sensor_name);
 
-		final SpringRestTask restReq = new SpringRestTask(ctx, handler);
-		restReq.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, SpringRestTask.RequestMethod.POST.toString(),
-				SpringRestTask.URI_USER_REGISTER, user);
-	}
+        final List<NameValuePair> uriData = RestUriFactory.getUri(RestUriType.SEND_SENSOR_READING, obj);
+
+        if (uriData.size() == 0) {
+            NocturneApplication.logMessage(Log.ERROR, LOG_TAG + "sendSensorReading() for " + obj.sensor_name);
+            return;
+        }
+
+        final SpringRestTask restReq = new SpringRestTask(ctx, handler);
+        restReq.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, SpringRestTask.RequestMethod.POST.toString(),
+                SpringRestTask.URI_SEND_SENSOR_READING, obj);
+    }
+
+    public void sendSubscriptionMessage(final Context ctx, final Handler handler, final UserDb user) {
+        NocturneApplication.logMessage(Log.INFO, LOG_TAG + "sendSubscriptionMessage() for " + user.getName_first());
+
+        final SpringRestTask restReq = new SpringRestTask(ctx, handler);
+        restReq.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, SpringRestTask.RequestMethod.POST.toString(),
+                SpringRestTask.URI_USER_REGISTER, user);
+    }
 }
