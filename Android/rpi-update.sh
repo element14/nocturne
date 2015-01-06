a5615a83539e72bc1e2995c6f0b6f5237002e1f5
andy <andyaspellclark@gmail.com>
2015-01-06 08:12:15 +0000

    moved dependencies into gradle files

---
+++ Android/rpi-update.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# ---------------------------------------------------------------------
+# Android Studio startup script.
+# ---------------------------------------------------------------------
+#
+ScriptName=`basename $0`
+TestServerUrl=https://github.com/element14/nocturne.git
+GitDir=~/nocturne
+
+echo
+echo "# executing script ["$ScriptName"]"
+echo "# path to me --------------->  ${0}     "
+echo "# parent path -------------->  ${0%/*}  "
+echo "# my name ------------------>  ${0##*/} "
+
+if [ -d "$GitDir" ]; then
+	echo
+	echo "updating exising repo"
+	echo
+	cd $GitDir
+	git pull
+else
+	echo
+	echo "checking out new repo"
+	echo
+	git clone $TestServerUrl ~/nocturne
+fi
+
+echo
+echo "Building TestServer classes"
+echo
+cd $GitDir/Android
+cp -F ./rpi-update.sh ${0}
+chmod +x ${0}
+#gradlew :TestServer:compileJava
+
+echo
+echo
+ls -lh /build/classes/main/com/nocturne/MockServer.class
+echo
+
+echo
+echo "finished updating TestServer"
+echo "to execute use trhe command [gradlew :TestServer:run]"
+echo
+echo "Executing TestServer"
+echo
+gradlew :TestServer:run
+
+
